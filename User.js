9c40977d2d2c67df2d7e56eac15f23e5ddccc447
olamideApplearn <olamide.lawal@applearn.tv>
2017-05-24 16:29:40 +0100

    Add files via upload

---
+++ User.js
@@ -0,0 +1,62 @@
+/**
+ * User.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    first_name : { type: 'string' },
+
+    last_name : { type: 'string' },
+
+    email : { type: 'string' },
+
+    password : { type: 'string' },
+      
+      posts:{
+          collection: 'post',
+          via:'_user'
+      },
+      
+      //override toJson method
+      toJSON : function () {
+          
+          //extract obj
+          let obj = this.toObject();
+          
+          //delete the password field
+          delete obj.password;
+          
+          //return obj
+          return obj;
+      }
+  },
+    
+    /*
+    *@param password
+    *@param encPassword
+    *@return {promise}
+    */
+    
+    checkPassword(password,encPassword){
+        const mPack = require ('machinepack-passwords')
+        //create a new promise
+        return new Promise ( (resolve,reject) => {
+            
+            mPack.checkPassword({
+                passwordAttempt:password,
+                encryptedPassword:encPassword
+            })
+            .exec({
+                error : err =>reject(err),
+                incorrect: () =>resolve(false),
+                success: () =>resolve(true)
+            })  
+        });
+    }
+};
+
